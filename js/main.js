e063147edb1c66a11162e1fd0503dab0a5cf71ab
Jennifer Sampayo <sampayoruizjennifer26@gmail.com>
2026-02-15 02:22:11 -0500

    Mejoras visuales: colores negro y dorado, slider automatico con dots, ajustes en carrito y botones y dos imagenes

--- js/main.js
+++ js/main.js
@@ -243,3 +243,17 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 });
+
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+let current = 0;
+
+setInterval(() => {
+  slides[current].classList.remove("active");
+  dots[current].classList.remove("active");
+
+  current = (current + 1) % slides.length;
+
+  slides[current].classList.add("active");
+  dots[current].classList.add("active");
+}, 4000);
